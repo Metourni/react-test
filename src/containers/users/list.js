09cd53a85e8af57c24afea742b70d3459e1b9e08
Metourni Noureddine <noureddine.metourni@edbridg.com>
2020-01-28 21:10:42 +0100

    Imlement redux

---
+++ src/containers/users/list.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function UsersListContainer(){
+    return (
+        <div>
+            <p>
+                User List
+            </p>
+            <ul>
+                <li>ll</li>
+            </ul>
+            <a className="App-link" href="">
+                Create new user
+            </a>
+        </div>
+    );
+}
+
+
+export default UsersListContainer
